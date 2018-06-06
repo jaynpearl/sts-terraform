69347c704ac25691c7758ee0d658e5b00eb30ddc
Jay Ng <jayng@seekasia.com>
2018-06-06 16:10:34 +0800

    update tf config

--- terraform/04ec2-codepipeline/main.tf
+++ terraform/04ec2-codepipeline/main.tf
@@ -4,8 +4,9 @@ provider "aws" {}
 resource "aws_instance" "t2-ec2-instance" {
   ami = "ami-c3a996bf"
   instance_type = "t2.micro"
+  subnet_id = "subnet-2ec73349"
   tags = {
-    Name = "terraform ec2"
+    Name = "terraform ec2 from codepipeline"
     Environment = "development"
   }
 }
